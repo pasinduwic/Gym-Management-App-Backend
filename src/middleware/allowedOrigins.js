faee61bb43d7ca0836be314638f38daba3a5b14f
Pasindu Wickramarachchi <pasindu0810@gmail.com>
2023-07-21 08:41:05 +0000

    Update allowedOrigins.js

--- src/middleware/allowedOrigins.js
+++ src/middleware/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "https://i87zzl.csb.app",
   "https://h7gkvt-3000.csb.app",
-  "https://frolicking-valkyrie-420899.netlify.app",
+  "https://csb-kjrgn3.netlify.app",
 ];
 
 export default allowedOrigins;
